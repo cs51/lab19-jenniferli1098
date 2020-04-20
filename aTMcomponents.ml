dae9a243e8a5a185de86889121a39f705d4f270b
jenniferli1098 <jenniferli1098@gmail.com>
2020-04-19 19:06:41 -0700

    save

---
+++ aTMcomponents.ml
@@ -0,0 +1,72 @@
+open Printf ;;
+open Scanf ;;
+module DB = Database ;;
+
+(* Customer account identifiers *)
+type id = int ;;
+
+(* Possible actions that an ATM customer can perform *)
+type action =  | Balance  (* balance inquiry *)
+               | Withdraw of int   (* withdraw an amount *)
+               | Deposit of int    (* deposit an amount *)
+               | Next              (* finish this customer and move on to the next one *)
+               | Finished          (* shut down the ATM and exit entirely *);;
+
+(* A specification of a customer name and initial balance *)
+type account_spec = {name : string; id : id; balance : int} ;;
+
+(*....................................................................
+ATM component behaviors. See .mli file for further documentation. *)
+  let initialize (init : account_spec list) : unit =
+  List.iter (fun {name; id; balance}
+                  -> DB.create id name;
+                  DB.update id balance) init;;
+
+  let rec acquire_id () : id =
+    printf "Enter customer id: ";
+    try
+      let id = read_int () in
+      ignore (DB.exists id); id
+    with
+    | Not_found
+    | Failure _ -> printf "Invalid id \n";
+                  acquire_id () ;;
+
+  let rec acquire_amount () : int =
+    printf "Enter amount: ";
+    try
+      let amount = read_int () in
+      if amount <= 0 then raise (Failure "amount is non-positive");
+      amount
+    with
+    | Failure _ -> printf "Invalid amount \n";
+                   acquire_amount () ;;
+
+    let rec acquire_act () : action =
+      printf "Enter action: (B) Balance (-) Withdraw (+) Deposit \
+              (=) Done (X) Exit: %!";
+      scanf " %c"
+            (fun char -> match char with
+                        | 'b' | 'B'        -> Balance
+                        | '/' | 'x' | 'X'  -> Finished
+                        | '='              -> Next
+                        | 'w' | 'W' | '-'  -> Withdraw (acquire_amount ())
+                        | 'd' | 'D' | '+'  -> Deposit (acquire_amount ())
+                        | _                -> printf "  invalid choice\n";
+                                              acquire_act () ) ;;
+
+    let get_balance : id -> int = DB.balance ;;
+
+    let get_name : id -> string = DB.name ;;
+
+    let update_balance : id -> int -> unit = DB.update ;;
+
+    let present_message (m : string) : unit =
+      printf "%s\n%!" m ;;
+
+    let deliver_cash (amount : int) : unit =
+      printf "Here's your cash: ";
+      for _i = 1 to (amount / 20) do
+        printf "[20 @ 20]"
+      done;
+      printf " and %d more\n" (amount mod 20) ;;
